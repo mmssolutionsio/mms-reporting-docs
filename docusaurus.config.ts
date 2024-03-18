b05cada2ef590734c055ccde06c6b8ad92533282
Florian B. Hochstatter <florianhochstatter@gmail.com>
2024-03-18 10:06:25 +0100

    [FEATURE] init project

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,16 +8,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://multivisio.github.io/mms-reporting-docs',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/mms-reporting-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'multivisio', // Usually your GitHub org/user name.
   projectName: 'mms-reporting-docs', // Usually your repo name.
-  deploymentBranch: 'production',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
