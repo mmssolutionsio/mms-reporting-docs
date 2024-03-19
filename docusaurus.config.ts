7223fbd9a90e56bc2bd568913766d68e3768e7a2
Florian Hochstatter <florianhochstatter@gmail.com>
2024-03-19 18:52:12 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,8 +11,8 @@ const config: Config = {
   url: 'https://multivisio.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  //baseUrl: '/mms-reporting-docs/',
-  baseUrl: '/',
+  baseUrl: '/mms-reporting-docs/',
+  //baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
